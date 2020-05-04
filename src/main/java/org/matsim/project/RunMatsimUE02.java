b307980e35728bb7ab5324e7901bdc09d86b1df9
altitudeflight <erik.stiegemann@me.com>
2020-05-04 18:39:10 +0200

    Alternative RunMatsim Klassen aus zweiter Übung und dritter Vorlesung

--- src/main/java/org/matsim/project/RunMatsimUE02.java
+++ src/main/java/org/matsim/project/RunMatsimUE02.java
@@ -22,7 +22,6 @@ import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.population.*;
-import org.matsim.contrib.otfvis.OTFVisLiveModule;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.Controler;
@@ -36,7 +35,7 @@ import java.util.List;
  * @author nagel
  *
  */
-public class RunMatsim{
+public class RunMatsimUE02 {
 
 	public static void main(String[] args) {
 
@@ -49,6 +48,48 @@ public class RunMatsim{
 		Scenario scenario = ScenarioUtils.loadScenario(config) ;
 
 		// possibly modify scenario here
+
+		// let MATsim output only 1 person
+
+		Id<Person> interestingPersonId = Id.createPersonId(1);
+		List<Id<Person>> personstoRemove = new ArrayList<>();
+
+		for (Id<Person> personId : scenario.getPopulation().getPersons().keySet()) {
+			if (!personId.equals(interestingPersonId)) {
+				personstoRemove.add(personId);
+			}
+		}
+
+		for (Id<Person> personId : personstoRemove) {
+			scenario.getPopulation().removePerson(personId);
+		}
+
+		System.out.println("Population size = " + scenario.getPopulation().getPersons().size()); // should output 1
+
+		// create new person with a daily plan
+
+		PopulationFactory populationFactory = scenario.getPopulation().getFactory();
+
+		Person person2 = populationFactory.createPerson(Id.createPersonId("Erik"));
+
+		Plan plan = populationFactory.createPlan();
+
+		Activity homeActivity= populationFactory.createActivityFromLinkId("h", Id.createLinkId(21));
+		homeActivity.setEndTime(6*60+60.);
+		plan.addActivity(homeActivity);
+
+		Leg leg = populationFactory.createLeg(TransportMode.car);
+		plan.addLeg(leg);
+
+		Activity workActivity = populationFactory.createActivityFromLinkId("w", Id.createLinkId(1));
+		plan.addActivity(workActivity);
+
+		person2.addPlan(plan);
+
+		scenario.getPopulation().addPerson(person2);
+
+		System.out.println("Population size = " + scenario.getPopulation().getPersons().size()); //should output 2 as we now have 2 persons
+
 
 		// ---
 
--- src/main/java/org/matsim/project/RunMatsimVL03.java
+++ src/main/java/org/matsim/project/RunMatsimVL03.java
@@ -18,29 +18,32 @@
  * *********************************************************************** */
 package org.matsim.project;
 
-import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
-import org.matsim.api.core.v01.TransportMode;
-import org.matsim.api.core.v01.population.*;
-import org.matsim.contrib.otfvis.OTFVisLiveModule;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.OutputDirectoryHierarchy;
 import org.matsim.core.scenario.ScenarioUtils;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * @author nagel
  *
  */
-public class RunMatsim{
+public class RunMatsimVL03 {
 
 	public static void main(String[] args) {
-
+/*
 		Config config = ConfigUtils.loadConfig( args ) ;
+*/
+		Config config;
+		if ( args==null || args.length==0 || args[0]==null ){
+			config = ConfigUtils.loadConfig( "scenarios/equil/config.xml" );
+		} else {
+			config = ConfigUtils.loadConfig( args );
+		}
+		config.controler().setOverwriteFileSetting(
+				OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists );
+
 
 		// possibly modify config here
 
@@ -55,7 +58,11 @@ public class RunMatsim{
 		Controler controler = new Controler( scenario ) ;
 
 		// possibly modify controler here
-
+/*
+		Controler controler = new Controler( scenario ) ;
+		controler.addOverridingModule( new OTFVisLiveModule() ) ;
+		controler.run();
+*/
 		controler.run();
 	}
 	
